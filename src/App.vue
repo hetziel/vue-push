9cfa43ac1ac1bda267851b71ec2d66398ca04158
Hetziel <hetzielmalave@gmail.com>
2025-09-16 22:45:53 -0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+onMounted(() => {
+  const notifyBtn = document.getElementById('notify-btn');
+
+notifyBtn.addEventListener('click', () => {
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+        new Notification("Hi old there!");
+    } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+            if (permission === "granted") {
+                new Notification("Hi new there!");
+            }
+        });
+    }
+});
+}),
+</script>
+
+<template>
+  <h1>Push Notifications Example</h1>
+    <button id="notify-btn">Show Notification</button>
+</template>
+
+<style scoped></style>
